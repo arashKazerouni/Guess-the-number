80dba70340f560b543cdc06615b8f8f3164029d9
arashKazerouni <94610980+arashKazerouni@users.noreply.github.com>
2022-05-28 04:16:53 +0430

    Implementing the game Logic

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 'use strict';
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 10;
+
+document.querySelector('.number').textContent = secretNumber;
+
+document.querySelector('.check').addEventListener('click', () => {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess);
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔No Number';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '✅ Correct Number';
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📈Too High';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '❌You lost the game';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📉Too low';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '❌You lost the game';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
