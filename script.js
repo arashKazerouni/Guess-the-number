68f413ed729cd36faf2658bc1268a50f77e89eec
arashKazerouni <94610980+arashKazerouni@users.noreply.github.com>
2022-05-29 03:44:53 +0430

    Manipulating css + coding challange

--- script.js
+++ script.js
@@ -1,17 +1,31 @@
 'use strict';
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
-let score = 10;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
 
-document.querySelector('.number').textContent = secretNumber;
+document.querySelector('.again').addEventListener('click', () => {
+  document.querySelector('.message').textContent = 'Start guessing..';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+  score = 20;
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.guess').textContent = '';
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('.number').textContent = '?';
+});
 
 document.querySelector('.check').addEventListener('click', () => {
   const guess = Number(document.querySelector('.guess').value);
-  console.log(guess);
+  // WHEN THERE IS NO INPUT
   if (!guess) {
     document.querySelector('.message').textContent = '⛔No Number';
+    // WHEN PLAYER WINS
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '✅ Correct Number';
+    document.querySelector('.number').textContent = secretNumber;
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+    // WHEN GUESS IS TOO HIGH
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '📈Too High';
@@ -21,6 +35,7 @@ document.querySelector('.check').addEventListener('click', () => {
       document.querySelector('.message').textContent = '❌You lost the game';
       document.querySelector('.score').textContent = 0;
     }
+    // WHEN GUESS IS TOO LOW
   } else if (guess < secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '📉Too low';
