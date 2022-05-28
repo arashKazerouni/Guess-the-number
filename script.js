7d0af600184072bb834166116a8be865c1fd6257
arashKazerouni <94610980+arashKazerouni@users.noreply.github.com>
2022-05-29 03:55:26 +0430

    Implementing highscores

--- script.js
+++ script.js
@@ -2,17 +2,8 @@
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highscore = 0;
 
-document.querySelector('.again').addEventListener('click', () => {
-  document.querySelector('.message').textContent = 'Start guessing..';
-  document.querySelector('body').style.backgroundColor = '#222';
-  document.querySelector('.number').style.width = '15rem';
-  score = 20;
-  document.querySelector('.score').textContent = score;
-  document.querySelector('.guess').textContent = '';
-  secretNumber = Math.trunc(Math.random() * 20) + 1;
-  document.querySelector('.number').textContent = '?';
-});
 
 document.querySelector('.check').addEventListener('click', () => {
   const guess = Number(document.querySelector('.guess').value);
@@ -25,6 +16,11 @@ document.querySelector('.check').addEventListener('click', () => {
     document.querySelector('.number').textContent = secretNumber;
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
     // WHEN GUESS IS TOO HIGH
   } else if (guess > secretNumber) {
     if (score > 1) {
@@ -46,4 +42,16 @@ document.querySelector('.check').addEventListener('click', () => {
       document.querySelector('.score').textContent = 0;
     }
   }
+});
+
+// If Player Click on Again !
+document.querySelector('.again').addEventListener('click', () => {
+  document.querySelector('.message').textContent = 'Start guessing..';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+  score = 20;
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.guess').value = '';
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('.number').textContent = '?';
 });
